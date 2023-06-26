5bcd36f058bac41ef83d0a2ac0c7ced2f1a009af
Ian Hogers <hogers@Ians-Mac-mini.local>
2023-06-27 09:23:25 +1000

    feat[Nav]: Started header nav

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,28 @@
+<script>
+    import favIcon from "/images/favicon.png";
+</script>
+
+<div class="nav-wrapper">
+    <img src={favIcon} alt="Profile Shot" />
+    <nav>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/solutions">Solutions</a>
+    </nav>
+</div>
+
+<style>
+    .nav-wrapper {
+        display: flex;
+        align-items: center;
+    }
+
+    nav {
+        width: 100%;
+        padding: 0px 24px;
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+    import Nav from "$lib/components/Nav.svelte";
+</script>
+
+<svelte:head>
+    <title>Ian Hogers - Software Engineer</title>
+</svelte:head>
+
+<Nav />
+<slot />
+
+<style>
+    :global(*) {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: "Commissioner", sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+    :global(body) {
+        background-color: hsl(216, 5%, 18%);
+        color: #fff;
+    }
+
+    :global(h1, h2, h3, h4, h5, h6) {
+        line-height: 2rem;
+        font-family: "Source Code Pro", monospace;
+        letter-spacing: 0.5px;
+    }
+</style>
