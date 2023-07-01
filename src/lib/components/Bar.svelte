2ada1069816bf78d922f9e2874270398da6064c0
Ian Hogers <ianhogers@hotmail.com>
2023-07-01 12:39:57 +1000

    feat[unplugin-icons]: added icon loader

---
+++ src/lib/components/Bar.svelte
@@ -0,0 +1,12 @@
+<div class="bar" />
+
+<style lang="scss">
+    @import "$styles/theme.scss";
+    .bar {
+        width: 100%;
+        height: $padding;
+        background-color: $color--green;
+        border-radius: 4px;
+        margin-top: $padding * 2;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-    import Container from "$lib/components/Container.svelte";
-    import Grid from "$lib/components/Grid.svelte";
-    import Spacer from "$lib/components/Spacer.svelte";
+    /**********************************************************************************************************
+     *   BASE IMPORT
+     **********************************************************************************************************/
+    import SolarCartLarge2BoldDuotone from "~icons/solar/cart-large-2-bold-duotone";
+    /**********************************************************************************************************
+     *   SHARED
+     **********************************************************************************************************/
+    import Bar from "$components/Bar.svelte";
+    import Container from "$components/Container.svelte";
+    import Grid from "$components/Grid.svelte";
+    import Spacer from "$components/Spacer.svelte";
+
+    /**********************************************************************************************************
+     *   CONSTS
+     **********************************************************************************************************/
     import ProfilePhoto from "/images/profile.jpeg";
 </script>
 
@@ -18,16 +30,25 @@
                 <h2>Ian Hogers</h2>
                 <h1>
                     Software Engineer
-                    <div class="bar" />
+                    <Bar />
                 </h1>
                 <h3>Building Solutions to your needs</h3>
             </div>
         </Grid>
     </Spacer>
+    <Spacer>
+        <div class="solutions">
+            <Grid columns="1fr 6fr">
+                <SolarCartLarge2BoldDuotone width={40} height={40} />
+                test
+            </Grid>
+        </div>
+    </Spacer>
 </Container>
 
 <style lang="scss">
     @import "$styles/theme.scss";
+
     .info {
         display: flex;
         flex-direction: column;
@@ -36,13 +57,5 @@
         > * {
             align-self: baseline;
         }
-    }
-
-    .bar {
-        width: 100%;
-        height: $padding;
-        background-color: $color--green;
-        border-radius: 4px;
-        margin-top: $padding * 2;
     }
 </style>
