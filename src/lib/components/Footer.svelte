9420881ba7ab3ff79be4a8a7ac18de79a5af6175
Ian Hogers <ianhogers@hotmail.com>
2023-07-02 14:46:54 +1000

    feat[Footer]: sveltekit link

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,12 +1,25 @@
 <script lang="ts">
     /**********************************************************************************************************
+     *   BASE IMPORT
+     **********************************************************************************************************/
+    /********** Icons **********/
+    import TablerBrandSvelte from "~icons/tabler/brand-svelte";
+    /**********************************************************************************************************
      *   SHARED
      **********************************************************************************************************/
     import Flex from "./Flex.svelte";
+    import Anchor from "./Anchor.svelte";
 </script>
 
 <div class="Footer background on-background-text">
-    <Flex>Footer :)</Flex>
+    <Flex>
+        Build with
+        <Anchor href="https://kit.svelte.dev/">
+            <Flex>
+                SvelteKit <TablerBrandSvelte />
+            </Flex>
+        </Anchor>
+    </Flex>
 </div>
 
 <style lang="scss">
