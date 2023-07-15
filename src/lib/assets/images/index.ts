b3389343f648c6e609c2b7a6886d1b63ed62084d
Ian Hogers <ianhogers@hotmail.com>
2023-07-15 19:41:06 +1000

    feat[Image]: Eatview image

--- src/lib/assets/images/index.ts
+++ src/lib/assets/images/index.ts
@@ -1,6 +1,12 @@
 import profilePhoto from "$lib/assets/images/profile.jpeg?run";
+import eatviewLogo from "$lib/assets/images/projects/eatview-icon.png?run";
 
 export const IMAGE_profileShot = {
     src: profilePhoto,
     alt: "Profile Shot",
+};
+
+export const IMAGE_eatviewLogo = {
+    src: eatviewLogo,
+    alt: "EatView Logo",
 };
