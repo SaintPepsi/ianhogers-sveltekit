6f9f3e48f138d7893e1a8665088360b8729c08d1
Ian Hogers <ianhogers@hotmail.com>
2023-07-15 21:57:45 +1000

    feat[projects/nav_title]: base md render

--- src/lib/data/projects/eatview/index.ts
+++ src/lib/data/projects/eatview/index.ts
@@ -1,12 +1,12 @@
 import { IMAGE_eatviewLogo } from "$lib/assets/images";
-import { Tags, type Project } from "$lib/data/projects/projects";
+import { Tags, type ProjectData } from "$lib/data/projects/projects";
 
-// import eatviewPageContent from "./Eatview ad20323de76f492eabaeebb1373fbb6e.md?raw";
+import eatviewPageContent from "./Eatview ad20323de76f492eabaeebb1373fbb6e.md?raw";
 
-export const EATVIEW_PROJECT: Project = {
+export const EATVIEW_PROJECT: ProjectData = {
     name: "EatView",
     nav_title: "eatview",
     logo: IMAGE_eatviewLogo,
-    page_content: "eatviewPageContent",
+    page_content: eatviewPageContent,
     tags: [Tags.Lifestyle, Tags.In_App_Purchases, Tags.Cloud_Storage],
 };
--- src/routes/projects/[nav_title]/+page.svelte
+++ src/routes/projects/[nav_title]/+page.svelte
@@ -1,6 +1,39 @@
 <script lang="ts">
+    import Bar from "$lib/components/Bar.svelte";
+    /**********************************************************************************************************
+     *   SHARED
+     **********************************************************************************************************/
+    import Container from "$lib/components/Container.svelte";
+    import Markdown from "$lib/components/Markdown.svelte";
+    import Spacer from "$lib/components/Spacer.svelte";
+
+    /********** SHARED Styled **********/
+    import HeadingTileContent from "$lib/components/styled/HeadingTileContent.svelte";
+    import Img from "@zerodevx/svelte-img";
+
     export let data;
     console.log("data", data);
+    const type = "primary";
 </script>
 
 That's a subpage!
+<Container size="medium">
+    <Spacer>
+        <HeadingTileContent>
+            <Img
+                style={"width: 100%; height: auto; object-fit: cover;"}
+                slot="Tile"
+                {...data.logo}
+            />
+            <svelte:fragment slot="Content">
+                <h1>{data.full_name ?? data.name}</h1>
+                <Bar {type} />
+                <h3>Subtext</h3>
+            </svelte:fragment>
+        </HeadingTileContent>
+    </Spacer>
+</Container>
+
+<Container size="medium">
+    <Markdown markdown={data.page_content} />
+</Container>
