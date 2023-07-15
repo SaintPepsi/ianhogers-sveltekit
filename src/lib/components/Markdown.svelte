48e37fd034544bae286ab89c3e105b9b26af4afc
Ian Hogers <ianhogers@hotmail.com>
2023-07-15 21:57:24 +1000

    feat[MarkDown]: load md content into html

---
+++ src/lib/components/Markdown.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import MarkdownIt from "markdown-it";
+
+    export let markdown = "";
+    const md = new MarkdownIt();
+</script>
+
+<div class="Markdown">
+    {@html md.render(markdown)}
+</div>
