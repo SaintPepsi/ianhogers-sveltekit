9e4ececfce22124366c9be91127bbe17ced2786d
Ian Hogers <ianhogers@hotmail.com>
2023-08-20 09:10:01 +1000

    feat[Anchor]: hover animation for .Icon

--- src/lib/components/Anchor.svelte
+++ src/lib/components/Anchor.svelte
@@ -10,10 +10,17 @@
         color: inherit;
         @extend .label-medium;
         text-decoration: none;
+        :global(.Icon) {
+            transition: all 0.2s ease-in-out;
+        }
 
         &:hover,
         &:focus {
             text-decoration: underline;
+
+            :global(.Icon) {
+                transform: translate(0, -$padding);
+            }
         }
     }
 </style>
