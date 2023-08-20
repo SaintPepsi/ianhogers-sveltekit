79e20fd66a9491bfe38364cb6835435564488011
Ian Hogers <ianhogers@hotmail.com>
2023-08-20 20:42:08 +1000

    feat[NotionBlockImage]: Remove filler

--- src/lib/components/notion/NotionBlockImage.svelte
+++ src/lib/components/notion/NotionBlockImage.svelte
@@ -3,10 +3,7 @@
     import type { ImageBlockObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
     export let block: ImageBlockObjectResponse;
-    console.log("block", block);
 </script>
-
-image!
 
 {#if block.image.type === "file"}
     <img width="100%" height="100%" src={block.image.file.url} alt={getPlainTextFromRichText(block.image.caption)} />
